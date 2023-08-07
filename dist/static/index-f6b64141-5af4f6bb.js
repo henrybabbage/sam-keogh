9b96140801c0d222377d66120d7101e5d73d311b
Henry Babbage <hbabbage@gmail.com>
2023-08-07 10:43:28 +0200

    add eslint rule for rule of hooks

--- dist/static/index-f6b64141-5af4f6bb.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-c5edfd9b-6b4164ec.js";import{D as m}from"./sanity-96ba4202.js";import"./json-inspector-19090858.js";export{m as DocumentPaneProvider,t as default};
---
+++ dist/static/index-f6b64141-89b3a1db.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-c5edfd9b-f3b29c29.js";import{D as m}from"./sanity-65bf7bd0.js";import"./json-inspector-e5f49823.js";export{m as DocumentPaneProvider,t as default};
