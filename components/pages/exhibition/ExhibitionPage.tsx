3cc8a007e89419a7c888edf06e80395a615aa9bf
Henry Babbage <hbabbage@gmail.com>
2023-09-19 12:13:39 +0200

    scaffold exhibition page layouts

--- components/pages/exhibition/ExhibitionPage.tsx
+++ components/pages/exhibition/ExhibitionPage.tsx
@@ -1,4 +1,5 @@
 import { css } from '@/styled-system/css'
+import { aspectRatio, flex, grid, gridItem } from '@/styled-system/patterns'
 
 import type { ExhibitionPagePayload } from '@/types'
 
@@ -11,7 +12,14 @@ export default function ExhibitionPage({ data }: ExhibitionPageProps) {
     console.log({ exhibition })
     return (
         <main className={css({ minHeight: '100vh', height: '100vh', width: '100vw', maxWidth: '100vw', p: '12px', bg: '#FFF1E5' })}>
-            <div>Exhibition Slug Page</div>
+            <div className={grid({ columns: 12, gap: '0' })}>
+                <div className={gridItem({ colSpan: 4 })}>
+                    <div className={flex({ flexDirection: 'column', h: '100%' })}>Exhibition menu</div>
+                </div>
+                <div className={gridItem({ colSpan: 8 })}>
+                    <div className={aspectRatio()}>Exhibition preview images</div>
+                </div>
+            </div>
         </main>
     )
 }
--- components/pages/exhibitions/ExhibitionsPage.tsx
+++ components/pages/exhibitions/ExhibitionsPage.tsx
@@ -1,4 +1,5 @@
-import { css } from '@/styled-system/css'
+import { css, cx } from '@/styled-system/css'
+import { aspectRatio, flex, grid, gridItem, scrollable } from '@/styled-system/patterns'
 
 import type { ExhibitionsPagePayload } from '@/types'
 
@@ -11,7 +12,22 @@ export default function ExhibitionsPage({ data }: ExhibitionsPageProps) {
     console.log({ exhibitions })
     return (
         <main className={css({ minHeight: '100vh', height: '100vh', width: '100vw', maxWidth: '100vw', p: '12px', bg: '#FFF1E5' })}>
-            <div>Exhibitions</div>
+            <div className={grid({ columns: 12, gap: '0' })}>
+                <div className={gridItem({ colStart: 1, colEnd: 5, rowStart: 1, rowEnd: 3, alignSelf: 'center' })}>
+                    <h1 className={css({ fontFamily: 'azeretMono' })}>Exhibitions</h1>
+                </div>
+                <div className={gridItem({ colStart: 1, colEnd: 5, rowStart: 3, rowEnd: 13 })}>
+                    <div className={scrollable({ direction: 'vertical', hideScrollbar: true })}>
+                        <h1 className={css({ fontFamily: 'azeretMono' })}>Upcoming / Current</h1>
+                        <h1 className={css({ fontFamily: 'azeretMono' })}>Past</h1>
+                    </div>
+                </div>
+                <div className={gridItem({ colStart: 5, colEnd: 13, rowStart: 1, rowEnd: 13 })}>
+                    <div className={cx(flex({ flexDirection: 'column' }), scrollable({ direction: 'vertical', hideScrollbar: true }))}>
+                        <div className={aspectRatio()}>Exhibition preview images</div>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
