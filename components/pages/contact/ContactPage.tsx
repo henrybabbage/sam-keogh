aea67c455db1dd49c65abc5feab4cfa90b521e82
Henry Babbage <hbabbage@gmail.com>
2023-09-19 11:24:28 +0200

    add azeretMono to contact page

--- components/pages/contact/ContactPage.tsx
+++ components/pages/contact/ContactPage.tsx
@@ -13,9 +13,15 @@ export default function ContactPage({ data }: ContactPageProps) {
         <main className={css({ maxHeight: '100vh', height: '100vh', width: '100vw', maxWidth: '100vw', p: '12px', bg: '#FFF1E5' })}>
             <div className={flex({ direction: 'column', justifyContent: 'center', height: '100%' })}>
                 <div className={flex({ direction: 'row', align: 'center', justifyContent: 'space-between', pb: '12px' })}>
-                    <a href={`mailto:${email}`}>{email}</a>
-                    <a href={instagram?.url}>{instagram?.title}</a>
-                    <a href={gallery?.url}>{gallery?.title}</a>
+                    <a className={css({ fontFamily: 'azeretMono' })} href={`mailto:${email}`}>
+                        {email}
+                    </a>
+                    <a className={css({ fontFamily: 'azeretMono' })} href={instagram?.url}>
+                        {instagram?.title}
+                    </a>
+                    <a className={css({ fontFamily: 'azeretMono' })} href={gallery?.url}>
+                        {gallery?.title}
+                    </a>
                 </div>
             </div>
         </main>
