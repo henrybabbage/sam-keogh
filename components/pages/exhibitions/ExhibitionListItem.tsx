e6caf05b4fcc2567072ae57864e5c17ac675b59c
Henry Babbage <hbabbage@gmail.com>
2023-09-19 15:30:13 +0200

    add date-fns formatting

--- components/pages/exhibitions/ExhibitionListItem.tsx
+++ components/pages/exhibitions/ExhibitionListItem.tsx
@@ -1,4 +1,6 @@
-import { ExhibitionProps } from '@/types'
+import { css } from '@/styled-system/css'
+import type { ExhibitionProps } from '@/types'
+import { format } from 'date-fns'
 
 export type ListItemProps = {
     exhibition: ExhibitionProps
@@ -8,10 +10,11 @@ export default function ExhibitionListItem(props: ListItemProps) {
     const { exhibition } = props
     return (
         <div>
-            <h3>{exhibition.title}</h3>
-            <h3>
-                <span>{exhibition.startDate}</span>
-                <span>{exhibition.endDate}</span>
+            <h3 className={css({ fontFamily: 'azeretMono' })}>{exhibition.title}</h3>
+            <h3 className={css({ fontFamily: 'azeretMono' })}>
+                <span>{format(new Date(exhibition.startDate), 'dd MMM')}</span>
+                <span>{' — '}</span>
+                <span>{format(new Date(exhibition.endDate), 'dd MMM yyyy')}</span>
             </h3>
         </div>
     )
