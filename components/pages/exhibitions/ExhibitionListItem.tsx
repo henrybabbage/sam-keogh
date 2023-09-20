38a774edfe543a2bffcebe13c381fb6aaa57492d
Henry Babbage <hbabbage@gmail.com>
2023-09-20 15:36:13 +0200

    add condition for no date given

--- components/pages/exhibitions/ExhibitionListItem.tsx
+++ components/pages/exhibitions/ExhibitionListItem.tsx
@@ -9,20 +9,22 @@ export type ListItemProps = {
 export default function ExhibitionListItem(props: ListItemProps) {
     const { exhibition } = props
     return (
-        <div>
+        <li>
             <h3 className={css({ fontFamily: 'azeretMono', fontStyle: 'italic' })}>{exhibition?.title}</h3>
-            {exhibition && exhibition.venue && (
+            {exhibition.venue && (
                 <h3 className={css({ fontFamily: 'azeretMono', fontStyle: 'normal', fontSize: 'md' })}>
                     {exhibition.venue.name && <span>{exhibition.venue.city ? exhibition.venue.name + ', ' : exhibition.venue.name}</span>}
                     {exhibition.venue.city && <span>{exhibition.venue.country ? exhibition.venue.city + ', ' : exhibition.venue.city}</span>}
                     {exhibition.venue.country && <span>{exhibition.venue.country}</span>}
                 </h3>
             )}
-            <h3 className={css({ fontFamily: 'azeretMono', fontStyle: 'italic' })}>
-                <span>{format(new Date(exhibition?.startDate), 'dd MMM')}</span>
-                <span>{' — '}</span>
-                <span>{format(new Date(exhibition?.endDate), 'dd MMM yyyy')}</span>
-            </h3>
-        </div>
+            {exhibition.startDate && exhibition.endDate && (
+                <h3 className={css({ fontFamily: 'azeretMono', fontStyle: 'italic' })}>
+                    <span>{format(new Date(exhibition?.startDate), 'dd MMM')}</span>
+                    <span>{' — '}</span>
+                    <span>{format(new Date(exhibition?.endDate), 'dd MMM yyyy')}</span>
+                </h3>
+            )}
+        </li>
     )
 }
