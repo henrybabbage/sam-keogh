1f0a9bf8a43a516db9bea7eb10eaa8b6d2bed589
Henry Babbage <hbabbage@gmail.com>
2023-09-20 18:44:19 +0200

    add aspect ratio class not pattern

--- components/pages/exhibitions/ExhibitionPreview.tsx
+++ components/pages/exhibitions/ExhibitionPreview.tsx
@@ -1,10 +1,9 @@
 // import { DynamicImage } from '@/components/common/DynamicImage'
 import { css, cx } from '@/styled-system/css'
-import { aspectRatio } from '@/styled-system/patterns'
 
 export default function ExhibitionPreview() {
     return (
-        <div className={cx(aspectRatio({ ratio: 16 / 9 }), css({ position: 'relative', mt: '14vh' }))}>
+        <div className={css({ position: 'relative', mt: '14vh', aspectRatio: '16/9' })}>
             <div className={css({ w: '100%', bg: 'blue.800' })}></div>
             {/* <DynamicImage
                 asset={''}
