8fe34e9eb1ca242a7d67a82c5806ab20fccf888d
Henry Babbage <hbabbage@gmail.com>
2023-09-23 21:26:39 +0200

    add  runtime = 'edge' to exhibitions page

--- app/(website)/exhibitions/page.tsx
+++ app/(website)/exhibitions/page.tsx
@@ -2,6 +2,8 @@ import ExhibitionsPage from '@/components/pages/exhibitions/ExhibitionsPage'
 import { getAllExhibitions } from '@/sanity/lib/sanity.fetch'
 import { notFound } from 'next/navigation'
 
+export const runtime = 'edge'
+
 export default async function Exhibitions() {
     const data = await getAllExhibitions()
     if (!data) {
